2e043e874f80f6a2e49a1f4c1871818b2cfc0b8d
MM <mm@inlead.dk>
2014-09-02 17:31:37 +0300

    DDBEASY 203 - Hide Submit button for My files and Library on DAMS page.

---
+++ js/dams_media_manager.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * Simple script for dams page.
+ */
+
+(function($) {
+  $(document).ready(function() {
+    $('#media-tab-media_default--media_browser_1 > div.form-actions.form-wrapper > a').hide();
+    $('#media-tab-media_default--media_browser_my_files > div.form-actions.form-wrapper > a').hide();
+  });
+}(jQuery));
